67803591de5829b905103d51ab698bb27b16ff91
cse-devansh-sharma <cse.devansh.sharma@gmail.com>
2026-03-20 12:08:57 +0530

    Initial Setup Commit

---
+++ Timesheet and Leave Management System/timesheet-service/src/test/java/com/cap/timesheet/TimesheetServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cap.timesheet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimesheetServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
